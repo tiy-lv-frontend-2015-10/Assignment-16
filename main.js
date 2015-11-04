a9c99c2f766fd4ae1caa9135909d774c1cd3dca4
Kristie White <insanelynocturnal@gmail.com>
2015-11-03 19:04:55 -0800

    added people

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var StarWarsObject = function () {};
 
-    function ajaxCallPath (path, id, callback) {
+    function ajaxCallId (path, id, callback) {
     var url = "http://www.swapi.co/api/";
     url = url + path + "/" + id;
         /*if (path) {
@@ -21,10 +21,38 @@ var StarWarsObject = function () {};
     });
     console.log(url);
     };
-  
-ajaxCallPath("people", 1, function(people){
+
+ajaxCallId("people", 1, function(people){
     console.log(people);
-})
+});
+
+
+
+ function ajaxCallPath (path, callback) {
+    var url = "http://www.swapi.co/api/";
+    url = url + path + "/";
+        /*if (path) {
+            url = url + path + "/";
+        }
+        
+        if (id) {
+            url = url + id;
+        }*/
+            
+
+    $.ajax({
+         url: url,
+        method:'get',
+        dataType: 'JSON'
+    }).then(function(response){
+        callback(response);
+    });
+    console.log(url);
+    };
+  
+ajaxCallPath ("people", function(people) {
+    console.log(people);
+});
 
 
 //ajaxCall.person (function(person){
