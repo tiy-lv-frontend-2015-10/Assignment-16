d2a321fa98ef2ef12ab0f13c2a5c691b8310dad3
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-11-03 19:39:32 -0800

    made a way more succint version

--- main.js
+++ main.js
@@ -1,227 +1,85 @@
-var swObj= function() {
-	this.getPerson=person;
-	this.getPeople=people;
-	this.getFilm=film;
-	this.getFilms=films;
-	this.getPlanet=planet;
-	this.getPlanets=planets;
-	this.getSpecie=specie;
-	this.getSpecies=species;
-	this.getStarship=starship;
-	this.getStarships=starships;
-	this.getVehicle=vehicle;
-	this.getVehicles=vehicles;
+swObj= function() {
+
+};
+
+
+function ajaxCall(path, id, callback) {
+	var url="http://www.swapi.co/api/";
+
+	url=url+path+"/"+id;
+
+	$.ajax({
+		url: url,
+		method:'get',
+		dataType:'json'
+	}).then(function(response){
+		callback(response);
+	});
+
+
+
 }
 
-swObj.person = function (id,callback) {
-	
-	var getPersonHtml="http://swapi.co/api/people/"+id;
-
-	$.ajax({
-		url: getPersonHtml,
-		method:'get',
-		dataType:'json',
-
-	}).then(function(people){
-		callback(people);
-	});
-};
-
-
-swObj.person(1, function(person){
-	console.log(person);
+ajaxCall('people',1,function(x){
+	console.log(x);
+});
+ajaxCall('films',1,function(x){
+	console.log(x);
+});
+ajaxCall('planets',1,function(x){
+	console.log(x);
+});
+ajaxCall('species',1,function(x){
+	console.log(x);
+});
+ajaxCall('starships',2,function(x){
+	console.log(x);
+});
+ajaxCall('vehicles',7,function(x){
+	console.log(x);
 });
 
-swObj.people = function(id,callback) {
 
-	var getPeopleHtml="http://swapi.co/api/people/?page="+id;
+
+
+
+
+
+function ajaxGen(path,callback) {
+	var url="http://www.swapi.co/api/";
+
+	url=url+path;
 
 	$.ajax({
-		url: getPeopleHtml,
+		url: url,
 		method:'get',
 		dataType:'json'
-	}).then(function(people) {
-		callback(people);
+	}).then(function(response){
+		callback(response);
 	});
 
-};
 
-swObj.people(1,function(people){
-	console.log(people);
+
+}
+
+ajaxGen('people',function(x){
+	console.log(x);
 });
-
-
-swObj.film = function(id,callback) {
-
-	var getFilmHtml="http://swapi.co/api/films/"+id;
-
-	$.ajax({
-		url: getFilmHtml,
-		method:'get',
-		dataType:'json'
-	}).then(function(film){
-		callback(film);
-	});
-
-};
-
-swObj.film(1,function(film){
-	console.log(film);
-})
-
-
-
-
-swObj.films = function(callback) {
-
-	var getFilmsHtml="http://swapi.co/api/films/";
-
-	$.ajax({
-		url: getFilmsHtml,
-		method:'get',
-		dataType:'json'
-	}).then(function(films){
-		callback(films);
-	});
-
-};
-
-swObj.films(function(films){
-	console.log(films);
+ajaxGen('films',function(x){
+	console.log(x);
 });
-
-swObj.planets = function(page,callback) {
-	var getPlanetsHtml="http://swapi.co/api/planets/?page="+page;
-
-	$.ajax({
-		url:getPlanetsHtml,
-		method:'get',
-		dataType:'json'
-	}).then(function(planets){
-		callback(planets);
-	});
-};
-
-swObj.planets(1,function(planets){
-	console.log(planets);
+ajaxGen('planets',function(x){
+	console.log(x);
 });
-
-swObj.planet = function(id,callback) {
-	var getPlanetHtml="http://swapi.co/api/planets/"+id;
-
-	$.ajax({
-		url:getPlanetHtml,
-		method:'get',
-		dataType:'json'
-	}).then(function(planet){
-		callback(planet);
-	});
-};
-
-swObj.planet(1,function(planet){
-	console.log(planet);
+ajaxGen('species',function(x){
+	console.log(x);
 });
-
-
-swObj.specie = function(id,callback) {
-	var getSpecieHtml="http://swapi.co/api/species/"+id;
-
-	$.ajax({
-		url:getSpecieHtml,
-		method:'get',
-		dataType:'json',
-	}).then(function(specie){
-		callback(specie);
-	});
-};
-
-swObj.specie(1,function(species){
-	console.log(species);
+ajaxGen('starships',function(x){
+	console.log(x);
 });
-
-swObj.species = function(page,callback) {
-	var getSpeciesHtml="http://swapi.co/api/species/?page="+page;
-
-	$.ajax({
-		url:getSpeciesHtml,
-		method:'get',
-		dataType:'json',
-	}).then(function(species){
-		callback(species);
-	});
-};
-
-swObj.species(1,function(species){
-	console.log(species);
+ajaxGen('vehicles',function(x){
+	console.log(x);
 });
-
-swObj.starship = function(id,callback) {
-	var getStarshipHtml="http://swapi.co/api/starships/"+id;
-
-		$.ajax({
-		url:getStarshipHtml,
-		method:'get',
-		dataType:'json',
-	}).then(function(starship){
-		callback(starship);
-	});
-};
-
-swObj.starship(3,function(starship){
-	console.log(starship);
-});
-
-swObj.starships = function(page,callback) {
-	var getStarshipsHtml="http://swapi.co/api/starships/?page="+page;
-
-		$.ajax({
-		url:getStarshipsHtml,
-		method:'get',
-		dataType:'json',
-	}).then(function(starships){
-		callback(starships);
-	});
-};
-
-swObj.starships(1,function(starship){
-	console.log(starship);
-});
-
-
-swObj.vehicle = function(id,callback) {
-	var getVehicleHtml="http://swapi.co/api/vehicles/"+id;
-
-		$.ajax({
-		url:getVehicleHtml,
-		method:'get',
-		dataType:'json',
-	}).then(function(vehicle){
-		callback(vehicle);
-	});
-};
-
-swObj.vehicle(7, function(vehicle){
-	console.log(vehicle);
-});
-
-swObj.vehicles = function(page,callback) {
-	var getVehiclesHtml="http://swapi.co/api/vehicles/?page="+page;
-
-		$.ajax({
-		url:getVehiclesHtml,
-		method:'get',
-		dataType:'json',
-	}).then(function(vehicles){
-		callback(vehicles);
-	});
-};
-
-swObj.vehicles(1, function(vehicles){
-	console.log(vehicles);
-});
-
-
-
 
 
 // getPerson(id) // - Returns one person.
